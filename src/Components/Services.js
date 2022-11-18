c3e22d0d83047bf56885d18cbf01f9b243ac9c16
unknown <mehmetsaki789@gmail.com>
2022-11-18 10:20:34 +0600

    services implemented

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -9,7 +9,7 @@ const Services = () => {
     return (
         <div className='services'>
       
-      <Carousel>
+      <Carousel infiniteLoop >
         <div>
             <img  src={img1} alt="item1" />
             <p className='legend'>MEARN stack</p>
